1d6095635658cdf359cc4bab2e4b576a07937a54
Achim <achim.t@gmail.com>
2015-10-17 00:35:48 +0200

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -70,7 +70,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# is less interesting than Edith...
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
-		inputbox.send_keys(Keys.Enter)
+		inputbox.send_keys(Keys.ENTER)
 
 		# Francis gets his won unique URL
 		francis_list_url = self.browser.current_url
