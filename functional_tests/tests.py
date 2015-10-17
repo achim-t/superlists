43ced740f8534d7237d31f0df723b40ef48fa445
Achim <achim.t@gmail.com>
2015-10-17 16:14:36 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,7 +35,7 @@ class NewVisitorTest(LiveServerTestCase):
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
 		inputbox.send_keys('Buy peacock feathers')
-		
+
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feather" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
@@ -43,7 +43,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		# There is still a text box inviting her to add another item. She 
+		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
@@ -55,7 +55,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# Now a new user, Francis, comes along to the site.
 
-		## We use a new browser session to make sure that no information 
+		## We use a new browser session to make sure that no information
 		## of Edith's is coming through from cookies etc #
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
@@ -84,5 +84,26 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# Satisfied, they both go back to sleep
 		#self.browser.quit()
-		
 
+	def test_layout_and_styling(self):
+		# Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+
+		# She starts a new list and sees the input is nicely
+		# centered there too.
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
