d39b644477327027e5990e6e579c2d1d6a79f33d
Achim <achim.t@gmail.com>
2015-10-25 20:32:06 +0100

    untabified

--- lists/models.py
+++ lists/models.py
@@ -3,22 +3,22 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 
 class List(models.Model):
-	owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
-	def get_absolute_url(self):
-		return reverse('view_list', args=[self.id])
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
-	@property
-	def name(self):
-		return self.item_set.first().text
+    @property
+    def name(self):
+        return self.item_set.first().text
 
 class Item(models.Model):
-	text = models.TextField(default='')
-	list = models.ForeignKey(List, default=None)
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
-	class Meta:
-		ordering = ('id',)
-		unique_together = ('list', 'text')
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
 
-	def __str__(self):
-		return self.text
+    def __str__(self):
+        return self.text
